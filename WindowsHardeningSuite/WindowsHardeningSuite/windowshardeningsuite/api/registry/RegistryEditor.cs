f2e2a8f1aee14f53683573a352348c429ea5cf4e
MysteryMystery <jamesattfield1990@gmail.com>
2019-02-13 19:19:22 +0000

    Fixed Syntax

--- WindowsHardeningSuite/WindowsHardeningSuite/windowshardeningsuite/api/registry/RegistryEditor.cs
+++ WindowsHardeningSuite/WindowsHardeningSuite/windowshardeningsuite/api/registry/RegistryEditor.cs
@@ -11,7 +11,7 @@ namespace WindowsHardeningSuite.windowshardeningsuite.api.registry
     {
         public void SetKey(string regPath, string keyName, object value, RegistryValueKind registryValueKind)
         {
-            using (RegistryKey regKey = Registry.LocalMachine.OpenSubKey(regPath, true){
+            using (RegistryKey regKey = Registry.LocalMachine.OpenSubKey(regPath, true)){
                 if (regKey == null)
                     throw new Exception("Registry key not found!");
                 regKey.SetValue(keyName, value, registryValueKind);
