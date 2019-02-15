64644cecfee6782e3b3790b3e80c4e798bd8621c
MysteryMystery <jamesattfield1990@gmail.com>
2019-02-15 17:17:32 +0000

    Added Registry Object and builder

--- WindowsHardeningSuite/WindowsHardeningSuite/App.xaml.cs
+++ WindowsHardeningSuite/WindowsHardeningSuite/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WindowsHardeningSuite.windowshardeningsuite.api.config;
 
 namespace WindowsHardeningSuite
 {
@@ -13,5 +14,9 @@ namespace WindowsHardeningSuite
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
